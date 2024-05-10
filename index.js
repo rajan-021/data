1e843a38aaa4e06e6e0cae5ee94dc1d2778df8b1
rajan-021 <20.rajanmaurya@gmail.com>
2024-05-10 16:08:26 +0530

    first commit

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-const jsonServer = require("json-server"); // importing json-server library
-const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
-
+const jsonServer = require("json-server");
 const express = require('express');
 const cors = require('cors');
 
-// Enable CORS for all origins
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 8080;
+
+// Enable CORS
 server.use(cors());
 
 
+// Apply JSON Server middlewares
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
+
+
