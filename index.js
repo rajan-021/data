71a5980f4e126a642b7abcc86718de82ffa07ec8
rajan-021 <20.rajanmaurya@gmail.com>
2024-05-10 16:01:44 +0530

    first commit

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
+const express = require('express');
+const cors = require('cors');
+
+// Enable CORS for all origins
+server.use(cors());
+
+
 server.use(middlewares);
 server.use(router);
 
